85d8568b741f2115e467b73d18d5a1504f3bf1fa
peter lu <rambo_lu@hotmail.com>
2021-03-29 16:50:23 -0700

    first commit

---
+++ cypress/integration/examples/examples/simple.spec.js
@@ -0,0 +1,14 @@
+describe('Browser Actions',() =>{
+  it('should load correct url',() =>{
+    cy.visit('http://example.com/', { timeout: 10000 })
+  })
+
+  it('should check correct url',() =>{
+    cy.url().should('include','example.com')
+  })
+
+  it('should check for correct element on the page',() =>{
+    cy.get('h1').should('be.visible')
+  })
+
+})
